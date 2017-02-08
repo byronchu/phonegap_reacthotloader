76630fd4818453d0450961287178b826ef608990
Byron Chu <chu.byron@yahoo.com>
2017-02-08 14:00:28 +1100

    forgot git add

---
+++ src/components/Byron.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Byron = React.createClass({
+    render: function() {
+        return <div>Byron says Hello world from -- the  React Component called Byron!</div>;
+    }
+});
+
+
+export default Byron
